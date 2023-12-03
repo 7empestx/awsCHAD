36cfdd657d63f36f6df77e606b6b14ea47ed7a1d
Grant Starkman <grant.starkman@gmail.com>
2023-12-02 21:54:56 -0700

    cognito impl

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,6 +3,7 @@ import { S3Stack } from '../lib/S3/S3Stack';
 import { LambdaStack } from '../lib/Lambda/LambdaStack';
 import { APIGatewayStack } from '../lib/APIGateway/APIGatewayStack';
 import { CloudfrontStack } from '../lib/Cloudfront/CloudfrontStack';
+import { CognitoStack } from '../lib/Cognito/CognitoStack';
 
 const app = new cdk.App();
 
@@ -11,3 +12,4 @@ const s3Stack = new S3Stack(app, 'S3Stack');
 new LambdaStack(app, 'LambdaStack');
 new APIGatewayStack(app, 'APIGatewayStack');
 new CloudfrontStack(app, 'CloudfrontStack', s3Stack.myBucket);
+new CognitoStack(app, 'CognitoStack');
