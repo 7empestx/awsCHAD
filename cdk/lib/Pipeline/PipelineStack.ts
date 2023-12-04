c983c4305515e5bc423ac1bc7db845526db956fe
Grant Starkman <grant.starkman@gmail.com>
2023-12-03 21:50:29 -0700

    script update

--- cdk/lib/Pipeline/PipelineStack.ts
+++ cdk/lib/Pipeline/PipelineStack.ts
@@ -1,7 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-import { Repository } from 'aws-cdk-lib/aws-codecommit'; 
+import { Repository } from 'aws-cdk-lib/aws-codecommit';
 import { PipelineStage } from './PipelineStage';
 
 export class PipelineStack extends cdk.Stack {
@@ -13,17 +13,16 @@ export class PipelineStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'awsChad',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.codeCommit(repository, 'main'), 
+        input: CodePipelineSource.codeCommit(repository, 'main'),
         commands: [
           'cd cdk',
-          'npm ci',
-          'npm run build',
-          'cdk synth',
+          'npm install typescript',
+          'npx cdk synth'
         ],
         primaryOutputDirectory: 'cdk/cdk.out',
       })
     });
-     
+
     pipeline.addStage(new PipelineStage(this, "Alpha", {
       env: { account: "659946347679", region: "us-west-2" }
     }));
