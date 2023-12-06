11680101d0e0c299ebb84d8eee555548ef24ab2b
Grant Starkman <grant.starkman@gmail.com>
2023-12-05 19:12:23 -0700

    amplify impl

--- react-frontend/src/Login/index.tsx
+++ react-frontend/src/Login/index.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Amplify } from 'aws-amplify';
+import awsExports from './../../config/aws-exports';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolClientId: awsExports.USER_POOL_APP_CLIENT_ID,
+      userPoolId: awsExports.USER_POOL_ID,
+    }
+  }
+})
 
 export function LoginPage() {
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div>
+          <p>Welcome {user.username}</p>
+          <button onClick={signOut}>Sign out</button>
+        </div>
+      )}
+    </Authenticator>
   );
 }
