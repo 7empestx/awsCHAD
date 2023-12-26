4f05d0a97fd7be54934f74f1aebb30619416a9d9
Grant Starkman <grant.starkman@gmail.com>
2023-12-26 15:43:49 -0700

    Add .gitignore and update dependencies

--- react-frontend/config/aws-exports.js
+++
@@ -1,5 +0,0 @@
-export default {
-    "REGION": "us-west-1",
-    "USER_POOL_ID": "us-west-2_mEBuuZV7s",
-    "USER_POOL_APP_CLIENT_ID": "30mjjupnr8oft276dd4rvvi9j0"
-}
--- react-frontend/src/Login/index.tsx
+++ react-frontend/src/Login/index.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { Amplify } from 'aws-amplify';
-import awsExports from './../../config/aws-exports';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 Amplify.configure({
   Auth: {
     Cognito: {
-      userPoolClientId: awsExports.USER_POOL_APP_CLIENT_ID,
-      userPoolId: awsExports.USER_POOL_ID,
+      userPoolClientId: process.env.USER_POOL_APP_CLIENT_ID,
+      userPoolId: process.env.USER_POOL_ID, 
     }
   }
 })
 
 export function LoginPage() {
   return (
-    <Authenticator>
+    <Authenticator signUpAttributes={['phone_number']}>
       {({ signOut, user }) => (
         <div>
           <p>Welcome {user.username}</p>
