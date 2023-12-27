bc1970c75b2ac69f7439eda10fd4f6bc6470bdd5
Grant Starkman <grant.starkman@gmail.com>
2023-12-27 13:16:39 -0700

    bucketdeploytshoot

--- cdk/lib/S3/S3Stack.ts
+++ cdk/lib/S3/S3Stack.ts
@@ -1,10 +1,11 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { aws_s3 as s3 } from "aws-cdk-lib";
+import { aws_cloudfront as cloudfront, aws_s3 as s3 } from "aws-cdk-lib";
 import * as deploy from "aws-cdk-lib/aws-s3-deployment";
 
 export class S3Stack extends cdk.Stack {
   public readonly myBucket: s3.Bucket;
+  readonly distribution: cloudfront.CloudFrontWebDistribution;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -16,9 +17,11 @@ export class S3Stack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
 
+    /*
     new deploy.BucketDeployment(this, "DeployWebsite", {
       destinationBucket: this.myBucket,
       sources: [deploy.Source.asset("../react-frontend/public")],
     });
+    */
   }
 }
