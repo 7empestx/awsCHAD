e98461681d64ac182c998f630d528f66b3df3895
Grant Starkman <grant.starkman@gmail.com>
2023-12-27 21:07:22 -0700

    frontend stack rotation

---
+++ cdk/lib/FrontendStack/FrontendStack.ts
@@ -0,0 +1,73 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { aws_s3 as s3, aws_cloudfront as cloudfront, aws_route53 as route53, aws_route53_targets as targets } from "aws-cdk-lib";
+import * as deploy from "aws-cdk-lib/aws-s3-deployment";
+import { PolicyStatement, Effect, StarPrincipal } from "aws-cdk-lib/aws-iam";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
+
+export class FrontendStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create the S3 bucket
+    const myBucket = new s3.Bucket(this, "IntelliSmileDentalBucket", {
+      bucketName: "intellismiledentalbucket",
+      versioned: true,
+      websiteIndexDocument: "index.html",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    });
+
+    // Add a bucket policy
+    myBucket.addToResourcePolicy(
+      new PolicyStatement({
+        actions: ["s3:GetObject"],
+        effect: Effect.ALLOW,
+        principals: [new StarPrincipal()],
+        resources: [myBucket.arnForObjects("*")],
+      }),
+    );
+
+    // Create the ACM certificate
+    const hostedZone = route53.HostedZone.fromLookup(this, "HostedZone", {
+      domainName: "intellismiledental.com",
+    });
+
+    const siteCertificate = new acm.DnsValidatedCertificate(this, "SiteCertificate", {
+      domainName: "intellismiledental.com",
+      hostedZone: hostedZone,
+      region: "us-east-1",
+    });
+
+    // Create the CloudFront distribution
+    const distribution = new cloudfront.CloudFrontWebDistribution(this, "CloudFront", {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: myBucket,
+          },
+          behaviors: [{ isDefaultBehavior: true }],
+        },
+      ],
+      viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(siteCertificate, {
+        aliases: ["intellismiledental.com"],
+      }),
+    });
+
+    // Deploy website assets to the bucket
+    new deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [deploy.Source.asset("../react-frontend/public")],
+      destinationBucket: myBucket,
+      distribution,
+      distributionPaths: ["/*"],
+    });
+
+    // Create Route 53 record
+    new route53.ARecord(this, "AliasRecord", {
+      zone: hostedZone,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
+      recordName: "intellismiledental.com",
+    });
+  }
+}
+
--- cdk/lib/Pipeline/PipelineStage.ts
+++ cdk/lib/Pipeline/PipelineStage.ts
@@ -1,30 +1,19 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { APIGatewayStack } from "../APIGateway/APIGatewayStack";
-import { CloudfrontStack } from "../Cloudfront/CloudfrontStack";
 import { CognitoStack } from "../Cognito/CognitoStack";
 import { LambdaStack } from "../Lambda/LambdaStack";
-import { S3Stack } from "../S3/S3Stack";
-import { Route53Stack } from "../Route53/Route53Stack";
+import { FrontendStack } from "../FrontendStack/FrontendStack";
 
 export class PipelineStage extends cdk.Stage {
   constructor(scope: Construct, id: string, props?: cdk.StageProps) {
     super(scope, id, props);
 
-    const s3Stack = new S3Stack(this, "S3Stack");
+    new FrontendStack(this, "FrontendStack");
 
     new LambdaStack(this, "LambdaStack");
     new APIGatewayStack(this, "APIGatewayStack");
     new CognitoStack(this, "CognitoStack");
 
-    const cloudfrontStack = new CloudfrontStack(
-      this,
-      "CloudfrontStack",
-      s3Stack.myBucket,
-    );
-
-    new Route53Stack(this, "Route53Stack", {
-      distribution: cloudfrontStack.distribution,
-    });
   }
 }
